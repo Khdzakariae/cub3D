a9f8d0d3cdf58e107746b8c6ff337d27694b10ea
khdzakariae <elkhadirzakaria48@gmail.com>
2024-10-13 11:51:30 +0100

    Refactor: Update key mappings and screen dimensions

--- Includes/cub3d.h
+++ Includes/cub3d.h
@@ -23,22 +23,22 @@ typedef struct s_data
 	char	**tmp_map;
 }			t_data;
 
+
 void draw_player(t_data *data);
 void draw_game(t_data *data);
 int	handle_destroy_notify(t_data *data);
 int	ButtonRelease(int keysym, t_data *data);
 
-#define KEY_UP 126
-#define KEY_DOWN 125
-#define KEY_LEFT 123
-#define KEY_RIGHT 124
+#define KEY_UP 65362
+#define KEY_DOWN 65364
+#define KEY_LEFT 65361
+#define KEY_RIGHT 65363
 
-#define HEIGHT 720
-#define WIDTH 1080
+#define HEIGHT 800
+#define WIDTH 800
 
-#define img_width WIDTH / 40
-#define img_height HEIGHT / 40
-
+#define width_pixel  5
+#define height_pixel  5
 
 
 #endif
