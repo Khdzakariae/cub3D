efc100ad5834683dd05621b9c851bfc0725cd0e6
khdzakariae <elkhadirzakaria48@gmail.com>
2024-10-13 11:51:37 +0100

    Fix: Print keycode on key release
    Adds printf to key release function to print the keycode of the released key.

--- src/key_press.c
+++ src/key_press.c
@@ -1,20 +1,21 @@
 
-#include "cub3d.h"
+#include "../Includes/cub3d.h"
 
 int	handle_destroy_notify(t_data *data)
 {
 	mlx_destroy_window(data->mlx, data->win);
-	// mlx_destroy_display(data->mlx);
+	mlx_destroy_display(data->mlx);
 	free(data->mlx);
 	exit(0);
 }
 
 int	ButtonRelease(int keysym, t_data *data)
 {
+    printf("the key  is %d \n", keysym);
 	if (keysym == 53)
 	{
 		mlx_destroy_window(data->mlx, data->win);
-		// mlx_destroy_display(data->mlx);
+		mlx_destroy_display(data->mlx);
 		free(data->mlx);
 		exit(0);
 	}
