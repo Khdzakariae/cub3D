64611309efd9c6dc5b56f08dfeb8c385c35b0723
ZAKARIAE <elkhadirzakaria48@gmail.com>
2024-11-05 23:33:02 +0100

    Refactor: Remove unnecessary header includes, macros, and unused structs

--- src/main.c
+++ src/main.c
@@ -1,47 +1,5 @@
 #include "../Includes/cub3d.h"
 
-// #include <mlx.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#define TILE_SIZE 32
-#define MAP_NUM_ROWS 11
-#define MAP_NUM_COLS 15
-#define WINDOW_WIDTH (MAP_NUM_COLS * TILE_SIZE)
-#define WINDOW_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
-
-#define KEY_UP 126
-#define KEY_DOWN 125
-#define KEY_LEFT 123
-#define KEY_RIGHT 124
-#define KEY_ESC 53
-
-typedef struct s_map {
-	int		map_height;
-	int 	map_width;
-    char **grid;
-} t_map;
-
-typedef struct s_player {
-    double x;
-    double y;
-    double radius;
-    double turnDirection;
-    double walkDirection;
-    double rotationAngle;
-    double moveSpeed;
-    double rotationSpeed;
-} t_player;
-
-typedef struct s_data {
-    void *mlx;
-    void *win;
-    t_map *map;
-    t_player *player;
-} t_data;
-
-
 void	get_map_resolution(t_map *map)
 {
 	map->map_height = 0;
