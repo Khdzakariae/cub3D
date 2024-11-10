a363018e4b0e87027459e808cc4007e8a3d41dba
ZAKARIAE <elkhadirzakaria48@gmail.com>
2024-11-10 22:25:03 +0100

    Refactor: Introduce RaycastData structure to store raycasting parameters

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "../Includes/cub3d.h"
 
+// New structure to hold raycasting data
+
 void render_walls(t_data *data) {
     for (int i = 0; i < NUM_RAYS; i++) {
         float distance = rays[i].distance;
@@ -34,101 +36,211 @@ int has_wall_at(t_map *map, double x, double y) {
     return 0;
 }
 
-void castRay(t_data *data, float rayAngle, int stripId) {
-    rayAngle = normalizeAngle(rayAngle);
+void initRayData(t_data *data, float rayAngle, RaycastData *rayData) {
+    (void)data;
+    rayData->rayAngle = normalizeAngle(rayAngle);
+    rayData->isRayFacingDown = rayAngle > 0 && rayAngle < M_PI;
+    rayData->isRayFacingUp = !rayData->isRayFacingDown;
+    rayData->isRayFacingRight = rayAngle < 0.5 * M_PI || rayAngle > 1.5 * M_PI;
+    rayData->isRayFacingLeft = !rayData->isRayFacingRight;
+}
 
-    int isRayFacingDown = rayAngle > 0 && rayAngle < M_PI;
-    int isRayFacingUp = !isRayFacingDown;
+void castHorizontalRay(t_data *data, float rayAngle, RaycastData *rayData) {
+    rayData->yintercept = floor(data->player->y / TILE_SIZE) * TILE_SIZE;
+    rayData->yintercept += rayData->isRayFacingDown ? TILE_SIZE : 0;
+    rayData->xintercept = data->player->x + (rayData->yintercept - data->player->y) / tan(rayAngle);
+    rayData->ystep = TILE_SIZE * (rayData->isRayFacingUp ? -1 : 1);
+    rayData->xstep = TILE_SIZE / tan(rayAngle);
+    rayData->xstep *= (rayData->isRayFacingLeft && rayData->xstep > 0) ? -1 : 1;
+    rayData->xstep *= (rayData->isRayFacingRight && rayData->xstep < 0) ? -1 : 1;
 
-    int isRayFacingRight = rayAngle < 0.5 * M_PI || rayAngle > 1.5 * M_PI;
-    int isRayFacingLeft = !isRayFacingRight;
+    float nextHorzTouchX = rayData->xintercept;
+    float nextHorzTouchY = rayData->yintercept;
+    rayData->foundHorzHit = FALSE;
 
-    float xintercept, yintercept;
-    float xstep, ystep;
-
-    int foundHorzWallHit = FALSE;
-    float horzWallHitX = 0;
-    float horzWallHitY = 0;
-    int horzWallContent = 0;
-    yintercept = floor(data->player->y / TILE_SIZE) * TILE_SIZE;
-    yintercept += isRayFacingDown ? TILE_SIZE : 0;
-    xintercept = data->player->x + (yintercept - data->player->y) / tan(rayAngle);
-    ystep = TILE_SIZE;
-    ystep *= isRayFacingUp ? -1 : 1;
-
-    xstep = TILE_SIZE / tan(rayAngle);
-    xstep *= (isRayFacingLeft && xstep > 0) ? -1 : 1;
-    xstep *= (isRayFacingRight && xstep < 0) ? -1 : 1;
-    float nextHorzTouchX = xintercept;
-    float nextHorzTouchY = yintercept;
     while (nextHorzTouchX >= 0 && nextHorzTouchX <= WINDOW_WIDTH && nextHorzTouchY >= 0 && nextHorzTouchY <= WINDOW_HEIGHT) {
         float xToCheck = nextHorzTouchX;
-        float yToCheck = nextHorzTouchY + (isRayFacingUp ? -1 : 0);
+        float yToCheck = nextHorzTouchY + (rayData->isRayFacingUp ? -1 : 0);
         if (has_wall_at(data->map, xToCheck, yToCheck)) {
-            horzWallHitX = nextHorzTouchX;
-            horzWallHitY = nextHorzTouchY;
-            horzWallContent = data->map->grid[(int)floor(yToCheck / TILE_SIZE)][(int)floor(xToCheck / TILE_SIZE)];
-            foundHorzWallHit = TRUE;
+            rayData->horzHitX = nextHorzTouchX;
+            rayData->horzHitY = nextHorzTouchY;
+            rayData->horzWallContent = data->map->grid[(int)floor(yToCheck / TILE_SIZE)][(int)floor(xToCheck / TILE_SIZE)];
+            rayData->foundHorzHit = TRUE;
             break;
         } else {
-            nextHorzTouchX += xstep;
-            nextHorzTouchY += ystep;
+            nextHorzTouchX += rayData->xstep;
+            nextHorzTouchY += rayData->ystep;
         }
     }
-    int foundVertWallHit = FALSE;
-    float vertWallHitX = 0;
-    float vertWallHitY = 0;
-    int vertWallContent = 0;
-    xintercept = floor(data->player->x / TILE_SIZE) * TILE_SIZE;
-    xintercept += isRayFacingRight ? TILE_SIZE : 0;
-    yintercept = data->player->y + (xintercept - data->player->x) * tan(rayAngle);
-    xstep = TILE_SIZE;
-    xstep *= isRayFacingLeft ? -1 : 1;
-    ystep = TILE_SIZE * tan(rayAngle);
-    ystep *= (isRayFacingUp && ystep > 0) ? -1 : 1;
-    ystep *= (isRayFacingDown && ystep < 0) ? -1 : 1;
-    float nextVertTouchX = xintercept;
-    float nextVertTouchY = yintercept;
+}
+
+void castVerticalRay(t_data *data, float rayAngle, RaycastData *rayData) {
+    rayData->xintercept = floor(data->player->x / TILE_SIZE) * TILE_SIZE;
+    rayData->xintercept += rayData->isRayFacingRight ? TILE_SIZE : 0;
+    rayData->yintercept = data->player->y + (rayData->xintercept - data->player->x) * tan(rayAngle);
+    rayData->xstep = TILE_SIZE * (rayData->isRayFacingLeft ? -1 : 1);
+    rayData->ystep = TILE_SIZE * tan(rayAngle);
+    rayData->ystep *= (rayData->isRayFacingUp && rayData->ystep > 0) ? -1 : 1;
+    rayData->ystep *= (rayData->isRayFacingDown && rayData->ystep < 0) ? -1 : 1;
+
+    float nextVertTouchX = rayData->xintercept;
+    float nextVertTouchY = rayData->yintercept;
+    rayData->foundVertHit = FALSE;
+
     while (nextVertTouchX >= 0 && nextVertTouchX <= WINDOW_WIDTH && nextVertTouchY >= 0 && nextVertTouchY <= WINDOW_HEIGHT) {
-        float xToCheck = nextVertTouchX + (isRayFacingLeft ? -1 : 0);
+        float xToCheck = nextVertTouchX + (rayData->isRayFacingLeft ? -1 : 0);
         float yToCheck = nextVertTouchY;
         if (has_wall_at(data->map, xToCheck, yToCheck)) {
-            vertWallHitX = nextVertTouchX;
-            vertWallHitY = nextVertTouchY;
-            vertWallContent = data->map->grid[(int)floor(yToCheck / TILE_SIZE)][(int)floor(xToCheck / TILE_SIZE)];
-            foundVertWallHit = TRUE;
+            rayData->vertHitX = nextVertTouchX;
+            rayData->vertHitY = nextVertTouchY;
+            rayData->vertWallContent = data->map->grid[(int)floor(yToCheck / TILE_SIZE)][(int)floor(xToCheck / TILE_SIZE)];
+            rayData->foundVertHit = TRUE;
             break;
         } else {
-            nextVertTouchX += xstep;
-            nextVertTouchY += ystep;
+            nextVertTouchX += rayData->xstep;
+            nextVertTouchY += rayData->ystep;
         }
     }
-    float horzHitDistance = foundHorzWallHit
-        ? distanceBetweenPoints(data->player->x, data->player->y, horzWallHitX, horzWallHitY)
+}
+
+void calculateHitDistances(t_data *data, RaycastData *rayData) {
+    rayData->horzHitDistance = rayData->foundHorzHit
+        ? distanceBetweenPoints(data->player->x, data->player->y, rayData->horzHitX, rayData->horzHitY)
         : INT_MAX;
-    float vertHitDistance = foundVertWallHit
-        ? distanceBetweenPoints(data->player->x, data->player->y, vertWallHitX, vertWallHitY)
+    rayData->vertHitDistance = rayData->foundVertHit
+        ? distanceBetweenPoints(data->player->x, data->player->y, rayData->vertHitX, rayData->vertHitY)
         : INT_MAX;
-    if (vertHitDistance < horzHitDistance) {
-        rays[stripId].distance = vertHitDistance;
-        rays[stripId].wallHitX = vertWallHitX;
-        rays[stripId].wallHitY = vertWallHitY;
-        rays[stripId].wallHitContent = vertWallContent;
+}
+
+void storeRayResults(int stripId, RaycastData *rayData) {
+    if (rayData->vertHitDistance < rayData->horzHitDistance) {
+        rays[stripId].distance = rayData->vertHitDistance;
+        rays[stripId].wallHitX = rayData->vertHitX;
+        rays[stripId].wallHitY = rayData->vertHitY;
+        rays[stripId].wallHitContent = rayData->vertWallContent;
         rays[stripId].wasHitVertical = TRUE;
     } else {
-        rays[stripId].distance = horzHitDistance;
-        rays[stripId].wallHitX = horzWallHitX;
-        rays[stripId].wallHitY = horzWallHitY;
-        rays[stripId].wallHitContent = horzWallContent;
+        rays[stripId].distance = rayData->horzHitDistance;
+        rays[stripId].wallHitX = rayData->horzHitX;
+        rays[stripId].wallHitY = rayData->horzHitY;
+        rays[stripId].wallHitContent = rayData->horzWallContent;
         rays[stripId].wasHitVertical = FALSE;
     }
-    rays[stripId].rayAngle = rayAngle;
-    rays[stripId].isRayFacingDown = isRayFacingDown;
-    rays[stripId].isRayFacingUp = isRayFacingUp;
-    rays[stripId].isRayFacingLeft = isRayFacingLeft;
-    rays[stripId].isRayFacingRight = isRayFacingRight;
-    // mlx_pixel_put(data->mlx, data->win, (int)rays[stripId].wallHitX, (int)rays[stripId].wallHitY, 0xFFFFFF);
+
+    rays[stripId].rayAngle = rayData->rayAngle;
+    rays[stripId].isRayFacingDown = rayData->isRayFacingDown;
+    rays[stripId].isRayFacingUp = rayData->isRayFacingUp;
+    rays[stripId].isRayFacingLeft = rayData->isRayFacingLeft;
+    rays[stripId].isRayFacingRight = rayData->isRayFacingRight;
 }
+
+void castRay(t_data *data, float rayAngle, int stripId) {
+    RaycastData rayData;
+    
+    initRayData(data, rayAngle, &rayData);
+    castHorizontalRay(data, rayAngle, &rayData);
+    castVerticalRay(data, rayAngle, &rayData);
+    calculateHitDistances(data, &rayData);
+    storeRayResults(stripId, &rayData);
+}
+
+
+
+// void castRay(t_data *data, float rayAngle, int stripId) {
+//     rayAngle = normalizeAngle(rayAngle);
+
+//     int isRayFacingDown = rayAngle > 0 && rayAngle < M_PI;
+//     int isRayFacingUp = !isRayFacingDown;
+
+//     int isRayFacingRight = rayAngle < 0.5 * M_PI || rayAngle > 1.5 * M_PI;
+//     int isRayFacingLeft = !isRayFacingRight;
+
+//     float xintercept, yintercept;
+//     float xstep, ystep;
+
+//     int foundHorzWallHit = FALSE;
+//     float horzWallHitX = 0;
+//     float horzWallHitY = 0;
+//     int horzWallContent = 0;
+//     yintercept = floor(data->player->y / TILE_SIZE) * TILE_SIZE;
+//     yintercept += isRayFacingDown ? TILE_SIZE : 0;
+//     xintercept = data->player->x + (yintercept - data->player->y) / tan(rayAngle);
+//     ystep = TILE_SIZE;
+//     ystep *= isRayFacingUp ? -1 : 1;
+
+//     xstep = TILE_SIZE / tan(rayAngle);
+//     xstep *= (isRayFacingLeft && xstep > 0) ? -1 : 1;
+//     xstep *= (isRayFacingRight && xstep < 0) ? -1 : 1;
+//     float nextHorzTouchX = xintercept;
+//     float nextHorzTouchY = yintercept;
+//     while (nextHorzTouchX >= 0 && nextHorzTouchX <= WINDOW_WIDTH && nextHorzTouchY >= 0 && nextHorzTouchY <= WINDOW_HEIGHT) {
+//         float xToCheck = nextHorzTouchX;
+//         float yToCheck = nextHorzTouchY + (isRayFacingUp ? -1 : 0);
+//         if (has_wall_at(data->map, xToCheck, yToCheck)) {
+//             horzWallHitX = nextHorzTouchX;
+//             horzWallHitY = nextHorzTouchY;
+//             horzWallContent = data->map->grid[(int)floor(yToCheck / TILE_SIZE)][(int)floor(xToCheck / TILE_SIZE)];
+//             foundHorzWallHit = TRUE;
+//             break;
+//         } else {
+//             nextHorzTouchX += xstep;
+//             nextHorzTouchY += ystep;
+//         }
+//     }
+//     int foundVertWallHit = FALSE;
+//     float vertWallHitX = 0;
+//     float vertWallHitY = 0;
+//     int vertWallContent = 0;
+//     xintercept = floor(data->player->x / TILE_SIZE) * TILE_SIZE;
+//     xintercept += isRayFacingRight ? TILE_SIZE : 0;
+//     yintercept = data->player->y + (xintercept - data->player->x) * tan(rayAngle);
+//     xstep = TILE_SIZE;
+//     xstep *= isRayFacingLeft ? -1 : 1;
+//     ystep = TILE_SIZE * tan(rayAngle);
+//     ystep *= (isRayFacingUp && ystep > 0) ? -1 : 1;
+//     ystep *= (isRayFacingDown && ystep < 0) ? -1 : 1;
+//     float nextVertTouchX = xintercept;
+//     float nextVertTouchY = yintercept;
+//     while (nextVertTouchX >= 0 && nextVertTouchX <= WINDOW_WIDTH && nextVertTouchY >= 0 && nextVertTouchY <= WINDOW_HEIGHT) {
+//         float xToCheck = nextVertTouchX + (isRayFacingLeft ? -1 : 0);
+//         float yToCheck = nextVertTouchY;
+//         if (has_wall_at(data->map, xToCheck, yToCheck)) {
+//             vertWallHitX = nextVertTouchX;
+//             vertWallHitY = nextVertTouchY;
+//             vertWallContent = data->map->grid[(int)floor(yToCheck / TILE_SIZE)][(int)floor(xToCheck / TILE_SIZE)];
+//             foundVertWallHit = TRUE;
+//             break;
+//         } else {
+//             nextVertTouchX += xstep;
+//             nextVertTouchY += ystep;
+//         }
+//     }
+//     float horzHitDistance = foundHorzWallHit
+//         ? distanceBetweenPoints(data->player->x, data->player->y, horzWallHitX, horzWallHitY)
+//         : INT_MAX;
+//     float vertHitDistance = foundVertWallHit
+//         ? distanceBetweenPoints(data->player->x, data->player->y, vertWallHitX, vertWallHitY)
+//         : INT_MAX;
+//     if (vertHitDistance < horzHitDistance) {
+//         rays[stripId].distance = vertHitDistance;
+//         rays[stripId].wallHitX = vertWallHitX;
+//         rays[stripId].wallHitY = vertWallHitY;
+//         rays[stripId].wallHitContent = vertWallContent;
+//         rays[stripId].wasHitVertical = TRUE;
+//     } else {
+//         rays[stripId].distance = horzHitDistance;
+//         rays[stripId].wallHitX = horzWallHitX;
+//         rays[stripId].wallHitY = horzWallHitY;
+//         rays[stripId].wallHitContent = horzWallContent;
+//         rays[stripId].wasHitVertical = FALSE;
+//     }
+//     rays[stripId].rayAngle = rayAngle;
+//     rays[stripId].isRayFacingDown = isRayFacingDown;
+//     rays[stripId].isRayFacingUp = isRayFacingUp;
+//     rays[stripId].isRayFacingLeft = isRayFacingLeft;
+//     rays[stripId].isRayFacingRight = isRayFacingRight;
+// }
+    // mlx_pixel_put(data->mlx, data->win, (int)rays[stripId].wallHitX, (int)rays[stripId].wallHitY, 0xFFFFFF);
 
 void castAllRays(t_data *data) {
     float rayAngle = data->player->rotationAngle - (FOV_ANGLE / 2);
