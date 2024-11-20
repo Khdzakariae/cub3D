95cb2de78b2a868254bbc91d1d96a629eb532c4d
ZAKARIAE <elkhadirzakaria48@gmail.com>
2024-11-20 23:42:27 +0100

    Fix: Update commit message to follow conventions

---
+++ parse_cub3d/load_cub3d_map.c
@@ -0,0 +1,216 @@
+#include "main.h"
+
+void		load_cub3d_map(char **cube_file, t_cub3d **cub3d_data);
+void		fill_map(char **cube_file, t_cub3d **cub3d_data, size_t *i);
+void		copy_map_content(char **cube_file, t_cub3d **cub3d_data, size_t map_starting_i);
+void        map_is_valid(t_cub3d **cub3d_data);
+void        content_is_valid(t_cub3d **cub3d_data);
+void        fetch_map_width(t_cub3d **cub3d_data);
+
+void	load_cub3d_map(char **cube_file, t_cub3d **cub3d_data)
+{
+	bool map;
+	size_t i;
+
+	map = false;
+	i = 0;
+	while (cube_file[i] != NULL)
+	{
+		if (cube_file[i][0] == '1' && map == false)
+		{
+			map = true;
+			// fill the map on cub3d_data, and dirreference the index.
+			fill_map(cube_file, cub3d_data, &i);
+			// map contains all infos, it's surronded by walls.
+			map_is_valid(cub3d_data);
+			fetch_map_width(cub3d_data);
+            if (cube_file[i] == NULL || cube_file[i][0] == '\0')
+                break ;
+		}
+		else if (cube_file[i][0] == '1' && map == true)
+		{
+			printf("Error\nInvalid map\n");
+			// have to free what needs to be freed. TODO.
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+    printf("Map width: %d\n", (*cub3d_data)->map.width);
+    printf("Map height: %d\n", (*cub3d_data)->map.height);
+    // print map
+    for (size_t i = 0; i < (*cub3d_data)->map.height; i++)
+        printf("%s\n", (*cub3d_data)->map.map[i]);
+    if ((*cub3d_data)->map.width == 0 || (*cub3d_data)->map.height == 0)
+    {
+        printf("Error\nNo map\n");
+        // TODO free what needs to be freed.
+        exit(EXIT_FAILURE);
+    }
+}
+
+void	fill_map(char **cube_file, t_cub3d **cub3d_data, size_t *i)
+{
+	size_t map_starting_i;
+	size_t map_height;
+
+	map_starting_i = *i;
+	map_height = get_map_height(cube_file, map_starting_i);
+	*i += map_height;
+	(*cub3d_data)->map.height = map_height;
+	(*cub3d_data)->map.map = (char **)malloc(sizeof(char *) * map_height + 1);
+	if (!(*cub3d_data)->map.map)
+	{
+		printf("Error\nFailed to allocate memory\n");
+		// TODO free what needs to be freed.
+		exit(EXIT_FAILURE);
+	}
+	// fill the map
+		// skip spaces, and check for valid characters when filling the map.
+		// check if the map is surrounded by walls.
+	copy_map_content(cube_file, cub3d_data, map_starting_i);
+}
+
+void	copy_map_content(char **cube_file, t_cub3d **cub3d_data, size_t map_starting_i)
+{
+	size_t map_j;
+	size_t j;
+	size_t line_map_len;
+	size_t map_i;
+	size_t map_height_scaled;
+
+	line_map_len = 0;
+	map_i = 0;
+	map_height_scaled = (*cub3d_data)->map.height + map_starting_i;
+	while (map_starting_i < map_height_scaled)
+	{
+		map_j = 0;
+		j = 0;
+		get_map_line_len(cube_file[map_starting_i], &line_map_len);
+		(*cub3d_data)->map.map[map_i] = (char *)malloc(sizeof(char) * line_map_len + 1);
+		while (cube_file[map_starting_i][j] != '\0')
+		{
+			if (valid_map_char(cube_file[map_starting_i][j]) == false)
+			{
+				printf("Error\nInvalid character in map\n");
+				// TODO free what needs to be freed.
+				exit(EXIT_FAILURE);
+			}
+			if (ft_isspace(cube_file[map_starting_i][j]) == false)
+			{
+				(*cub3d_data)->map.map[map_i][map_j] = cube_file[map_starting_i][j];
+				map_j++;
+			}
+			j++;
+		}
+		(*cub3d_data)->map.map[map_i][map_j] = '\0';
+		map_i++;
+		map_starting_i++;
+	}
+	(*cub3d_data)->map.map[map_i] = NULL;
+}
+
+void    map_is_valid(t_cub3d **cub3d_data)
+{
+    // check if the map is surrounded by walls.
+    size_t i;
+    int     len_line;
+
+    i = 0;
+    while ((*cub3d_data)->map.map[i] != NULL)
+    {
+        len_line = ft_strlen((*cub3d_data)->map.map[i]);
+        if (len_line < 3)
+        {
+            printf("Error\nMap is not surrounded by walls\n");
+            // TODO free what needs to be freed.
+            exit(EXIT_FAILURE);
+        }
+        if (i == 0)
+        {
+            if (check_if_all_walls((*cub3d_data)->map.map[i]) == false)
+            {
+                printf("Error\nMap is not surrounded by walls\n");
+                // TODO free what needs to be freed.
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if ((*cub3d_data)->map.map[i][0] != '1' || (*cub3d_data)->map.map[i][len_line - 1] != '1')
+        {
+            printf("Error\nMap is not surrounded by walls\n");
+            // TODO free what needs to be freed.
+            exit(EXIT_FAILURE);
+        }
+        else if (i == (*cub3d_data)->map.height - 1)
+        {
+            if (check_if_all_walls((*cub3d_data)->map.map[i]) == false)
+            {
+                printf("Error\nMap is not surrounded by walls\n");
+                // TODO free what needs to be freed.
+                exit(EXIT_FAILURE);
+            }
+        }
+        i++;
+    }
+    // check if map's data is valid (0, 1, N, S, W, E).
+    // check if the map content is valid.
+    content_is_valid(cub3d_data);
+}
+
+void        content_is_valid(t_cub3d **cub3d_data)
+{
+    size_t i;
+    size_t j;
+    bool    player;
+    bool    spaces;
+
+    i = 0;
+    player = false;
+    spaces = false;
+    while ((*cub3d_data)->map.map[i] != NULL)
+    {
+        j = 0;
+        while ((*cub3d_data)->map.map[i][j] != '\0')
+        {
+            if ((*cub3d_data)->map.map[i][j] == 'N' || (*cub3d_data)->map.map[i][j] == 'S' 
+                || (*cub3d_data)->map.map[i][j] == 'W' || (*cub3d_data)->map.map[i][j] == 'E')
+            {
+                if (player == true)
+                {
+                    printf("Error\nMultiple player position\n");
+                    // TODO free what needs to be freed.
+                    exit(EXIT_FAILURE);
+                }
+                player = true;
+                // player position
+                (*cub3d_data)->player_x = j;
+                (*cub3d_data)->player_y = i;
+            }
+            else if ((*cub3d_data)->map.map[i][j] == '0')
+                spaces = true;
+            j++;
+        }
+        i++;
+    }
+    if (player == false || spaces == false)
+    {
+        printf("Error\nNo player position or no spaces\n");
+        // TODO free what needs to be freed.
+        exit(EXIT_FAILURE);
+    }
+}
+
+void    fetch_map_width(t_cub3d **cub3d_data)
+{
+    size_t i;
+    int     max_len;
+
+    max_len = 0;
+    i = 0;
+    while ((*cub3d_data)->map.map[i] != NULL)
+    {
+        if (ft_strlen((*cub3d_data)->map.map[i]) > max_len)
+            max_len = ft_strlen((*cub3d_data)->map.map[i]);
+        i++;
+    }
+    (*cub3d_data)->map.width = max_len;
+}
---
+++ parse_cub3d/parse_utils.c
@@ -0,0 +1,92 @@
+#include "main.h"
+
+bool		check_cub_extension(char *file);
+void		init_cub3d_data(t_cub3d **cub3d);
+int			get_map_height(char **cube_file, size_t map_starting_i);
+void		get_map_line_len(char *line, size_t *line_map_len);
+bool 		valid_map_char(char c);
+bool		check_if_all_walls(char *line);
+void 		free_2d_array(char **array);
+
+bool	check_cub_extension(char *file)
+{
+	while (file && *file)
+	{
+		if (*file == '.' && *(file + 1) == 'c' && *(file + 2) == 'u' && *(file + 3) == 'b' && *(file + 4) == '\0')
+			return (true);
+		file++;
+	}
+	return (false);
+}
+void	init_cub3d_data(t_cub3d **cub3d)
+{
+	(*cub3d)->textures.no.path = NULL;
+	(*cub3d)->textures.no.is_set = false;
+	(*cub3d)->textures.so.path = NULL;
+	(*cub3d)->textures.so.is_set = false;
+	(*cub3d)->textures.we.path = NULL;
+	(*cub3d)->textures.we.is_set = false;
+	(*cub3d)->textures.ea.path = NULL;
+	(*cub3d)->textures.ea.is_set = false;
+	(*cub3d)->colors.floor.color = -1;
+	(*cub3d)->colors.floor.is_set = false;
+	(*cub3d)->colors.ceiling.color = -1;
+	(*cub3d)->colors.ceiling.is_set = false;
+	(*cub3d)->map.map = NULL;
+	(*cub3d)->map.width = 0;
+	(*cub3d)->map.height = 0;
+	(*cub3d)->player_x = -1;
+	(*cub3d)->player_y = -1;
+}
+
+int	get_map_height(char **cube_file, size_t map_starting_i)
+{
+	size_t i;
+
+	i = map_starting_i;
+	while (cube_file[i] != NULL && cube_file[i][0] == '1')
+		i++;
+	return (i - map_starting_i);
+}
+
+void	get_map_line_len(char *line, size_t *line_map_len)
+{
+	size_t i;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (ft_isspace(line[i]) == false)
+			*line_map_len += 1;
+		i++;
+	}
+}
+
+bool	valid_map_char(char c)
+{
+	return (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'W' || c == 'E' || c == ' ');
+}
+
+bool	check_if_all_walls(char *line)
+{
+	while (*line)
+	{
+		if (*line != '1')
+			return (false);
+		line++;
+	}
+	return (true);
+}
+
+void	free_2d_array(char **array)
+{
+	size_t i;
+
+	i = 0;
+	while (array[i] != NULL)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
