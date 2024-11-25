bc15f8623f15c0c68239846d169cd19625c52dac
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-11-25 19:55:40 +0100

    Fix: Add cleanup function to handle memory leaks
    This commit adds a cleanup function to handle memory leaks, which were causing issues with textures and other resources in the game.

---
+++ merge/final/src/cleanup.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 19:28:59 by zel-khad          #+#    #+#             */
+/*   Updated: 2024/11/25 19:29:23 by zel-khad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../Includes/cub3d.h"
+
+
+void cleanup_textures(t_cub3d *game)
+{
+    free_2d_array(game->map.map);
+    free(game->textures.ea.path);
+    free(game->textures.we.path);
+    free(game->textures.no.path);
+    free(game->textures.so.path);
+}
+
+int cleanup(t_data *data)
+{
+    t_texture *texters;
+    
+    texters = &data->game.textures;
+    if (texters->ea.img_ptr)
+        mlx_destroy_image(data->mlx, texters->ea.img_ptr);
+    if (texters->no.img_ptr)
+        mlx_destroy_image(data->mlx, texters->no.img_ptr);
+    if (texters->so.img_ptr)
+        mlx_destroy_image(data->mlx, texters->so.img_ptr);
+    if (texters->we.img_ptr)
+        mlx_destroy_image(data->mlx, texters->we.img_ptr);
+    if (texters->we.img_ptr)
+        mlx_destroy_image(data->mlx, texters->ciel.img_ptr);
+    if (data->img.img_ptr)
+        mlx_destroy_image(data->mlx, data->img.img_ptr);
+    if (data->win)
+        mlx_destroy_window(data->mlx, data->win);
+    if (data->mlx)
+    {
+        mlx_destroy_display(data->mlx);
+        free(data->mlx);
+    }
+    cleanup_textures(&data->game);
+
+    exit(0);
+}
