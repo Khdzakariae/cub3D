b598c07e1dfa5e41b7e6d1d0bf9ea73df26d32f2
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-11-30 09:52:13 +0100

    Refactor: Add key and mouse events handling for side movement

--- merge/src/execution/key_events.c
+++ merge/src/execution/key_events.c
@@ -6,42 +6,53 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:27:25 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/11/29 19:27:21 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/11/30 09:11:08 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-int	key_press(int keycode, t_data *data)
-{
-	if (!data)
-		return (-1);
-	if (keycode == KEY_UP || keycode == KEY_W)
-		data->game.player.walkdirection = 1;
-	else if (keycode == KEY_DOWN || keycode == KEY_S)
-		data->game.player.walkdirection = -1;
-	else if (keycode == KEY_LEFT || keycode == KEY_A)
-		data->game.player.turndirection = -1;
-	else if (keycode == KEY_RIGHT || keycode == KEY_D)
-		data->game.player.turndirection = 1;
-	else if (keycode == KEY_ESC)
-		cleanup(data);
-	else if (keycode == KEY_ACTIVE_MOUSSE)
-		data->flage_mousse *= -1;
-	return (0);
+int key_press(int keycode, t_data *data) {
+    if (!data) 
+        return (-1);
+    if (keycode == KEY_UP || keycode == KEY_W) {
+        data->game.player.walkdirection = 1;
+    }
+    else if (keycode == KEY_DOWN || keycode == KEY_S) {
+        data->game.player.walkdirection = -1;
+    }
+    else if (keycode == KEY_LEFT) {
+        data->game.player.turndirection = -1;
+    }
+    else if (keycode == KEY_RIGHT) {
+        data->game.player.turndirection = 1;
+    }
+    else if (keycode == KEY_A) {
+        data->game.player.sidedirection = -1;
+    }
+    else if (keycode == KEY_D) {
+        data->game.player.sidedirection = 1;
+    }
+    else if (keycode == KEY_ESC) {
+        cleanup(data);
+    }
+    return (0);
 }
 
-int	key_release(int keycode, t_data *data)
-{
-	if (!data)
-		return (-1);
-	if (keycode == KEY_UP || keycode == KEY_DOWN || keycode == KEY_W
-		|| keycode == KEY_S)
-		data->game.player.walkdirection = 0;
-	else if (keycode == KEY_LEFT || keycode == KEY_RIGHT || keycode == KEY_A
-		|| keycode == KEY_D)
-		data->game.player.turndirection = 0;
-	return (0);
+int key_release(int keycode, t_data *data) {
+    if (!data) 
+        return (-1);
+
+    if (keycode == KEY_UP || keycode == KEY_DOWN || keycode == KEY_W || keycode == KEY_S) {
+        data->game.player.walkdirection = 0;
+    }
+    else if (keycode == KEY_LEFT || keycode == KEY_RIGHT) {
+        data->game.player.turndirection = 0;
+    }
+    else if (keycode == KEY_A || keycode == KEY_D) {
+        data->game.player.sidedirection = 0;
+    }
+    return (0);
 }
 
 int	mouse_move(int x, int y, t_data *data)
