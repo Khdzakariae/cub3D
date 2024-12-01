f0663ad17fd70a322b8801c1b2ff07bf772d6f6e
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-12-01 15:26:00 +0100

    Refactor: Update main.c to include game menu

--- merge/src/execution/main.c
+++ merge/src/execution/main.c
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:44:09 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/12/01 09:40:48 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/12/01 15:16:45 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,12 @@ int	main(int argc, char **argv)
 		cleanup(&data, 2);
 		return (1);
 	}
-	render_player_1(&data);
-	mlx_put_image_to_window(data.mlx, data.win, data.img.img_ptr, 0, 0);
+	menu(&data);
 	sleep(2);
 	mlx_loop_hook(data.mlx, game_loop, &data);
 	mlx_hook(data.win, 2, 1L << 0, key_press, &data);
 	mlx_hook(data.win, 3, 1L << 1, key_release, &data);
 	mlx_hook(data.win, 6, 1L << 6, mouse_move, &data);
-	// mlx_hook(data.win, 17, 0, cleanup, &data);
 	mlx_loop(data.mlx);
 	return (0);
 }
