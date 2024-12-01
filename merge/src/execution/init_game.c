0d24d8416b84d9ce6431fd799a24e2d46ae54837
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-12-01 15:25:55 +0100

    Refactor: Remove unnecessary texture loading from init_game

--- merge/src/execution/init_game.c
+++ merge/src/execution/init_game.c
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:06:03 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/12/01 09:41:07 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/12/01 15:20:29 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,15 +40,13 @@ void	init_frames(t_data *data)
 			player->frames[i].image_pixel_ptr
 				= mlx_get_data_addr(player->frames[i].img_ptr,
 					&player->frames[i].bits_per_pixel,
-					&player->frames[i].line_len,
-					&player->frames[i].endian);
+					&player->frames[i].line_len, &player->frames[i].endian);
 		i++;
 	}
 }
 
 void	init_player(t_data *data)
 {
-	data->game.player.radius = 3;
 	data->game.player.turndirection = 0;
 	data->game.player.walkdirection = 0;
 	data->game.player.rotationangle = M_PI / 2;
@@ -58,71 +56,6 @@ void	init_player(t_data *data)
 	data->game.player.rotationspeed = 3 * (M_PI / 180);
 	data->game.last_mouse_x = 0;
 	data->game.player.sidedirection = 0;
-}
-
-void	init_textures_3(t_data *data)
-{
-	t_texture	*texters;
-
-	texters = &data->game.textures;
-	texters->menu.img_ptr = mlx_xpm_file_to_image(data->mlx, MENU,
-			&texters->menu.texter_with, &texters->menu.texter_height);
-	if (texters->menu.img_ptr)
-		texters->menu.image_pixel_ptr = mlx_get_data_addr(texters->menu.img_ptr,
-				&texters->menu.bits_per_pixel, &texters->menu.line_len,
-				&texters->menu.endian);
-	texters->ciel.img_ptr = mlx_xpm_file_to_image(data->mlx, CIEL,
-			&texters->ciel.texter_with, &texters->ciel.texter_height);
-	if (texters->ciel.img_ptr)
-		texters->ciel.image_pixel_ptr = mlx_get_data_addr(texters->ciel.img_ptr,
-				&texters->ciel.bits_per_pixel, &texters->ciel.line_len,
-				&texters->ciel.endian);
-}
-
-void	init_textures_2(t_data *data)
-{
-	t_texture	*texters;
-
-	texters = &data->game.textures;
-	texters->so.img_ptr = mlx_xpm_file_to_image(data->mlx, texters->so.path,
-			&texters->so.texter_with, &texters->so.texter_height);
-	if (texters->so.img_ptr)
-		texters->so.image_pixel_ptr = mlx_get_data_addr(texters->so.img_ptr,
-				&texters->so.bits_per_pixel, &texters->so.line_len,
-				&texters->so.endian);
-	texters->we.img_ptr = mlx_xpm_file_to_image(data->mlx, texters->we.path,
-			&texters->we.texter_with, &texters->we.texter_height);
-	if (texters->we.img_ptr)
-		texters->we.image_pixel_ptr = mlx_get_data_addr(texters->we.img_ptr,
-				&texters->we.bits_per_pixel, &texters->we.line_len,
-				&texters->we.endian);
-}
-
-void	init_textures(t_data *data)
-{
-	t_texture	*texters;
-
-	texters = &data->game.textures;
-	texters->ea.img_ptr = mlx_xpm_file_to_image(data->mlx, texters->ea.path,
-			&texters->ea.texter_with, &texters->ea.texter_height);
-	if (texters->ea.img_ptr)
-		texters->ea.image_pixel_ptr = mlx_get_data_addr(texters->ea.img_ptr,
-				&texters->ea.bits_per_pixel, &texters->ea.line_len,
-				&texters->ea.endian);
-	texters->no.img_ptr = mlx_xpm_file_to_image(data->mlx, texters->no.path,
-			&texters->no.texter_with, &texters->no.texter_height);
-	if (texters->no.img_ptr)
-		texters->no.image_pixel_ptr = mlx_get_data_addr(texters->no.img_ptr,
-				&texters->no.bits_per_pixel, &texters->no.line_len,
-				&texters->no.endian);
-	init_textures_2(data);
-	init_textures_3(data);
-	if ( !texters->ciel.img_ptr || !texters->ea.img_ptr || !texters->menu.img_ptr || !texters->no.img_ptr
-		|| !texters->so.img_ptr || !texters->we.img_ptr)
-	{
-		printf("ERROR : FAILED TO GET TEXTURES");
-		cleanup(data, 2);
-	}
 }
 
 void	init_game(t_data *data)
