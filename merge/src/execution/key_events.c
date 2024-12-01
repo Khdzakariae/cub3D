28278fd6e3214e08bd0472944eab55396243198c
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2024-12-01 15:25:57 +0100

    Refactor: Improve key and mouse event handling
    Changes functions' names for key and mouse events and adds initialization
    function for player movement.

--- merge/src/execution/key_events.c
+++ merge/src/execution/key_events.c
@@ -6,48 +6,68 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:27:25 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/12/01 09:40:59 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/12/01 15:04:39 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-int key_press(int keycode, t_data *data) {
-    if (!data) 
-        return (-1);
-    if (keycode == KEY_UP || keycode == KEY_W)
-        data->game.player.walkdirection = 1;
-    else if (keycode == KEY_DOWN || keycode == KEY_S)
-        data->game.player.walkdirection = -1;
-    else if (keycode == KEY_LEFT)
-        data->game.player.turndirection = -1;
-    else if (keycode == KEY_RIGHT)
-        data->game.player.turndirection = 1;
-    else if (keycode == KEY_A)
-        data->game.player.sidedirection = -1;
-    else if (keycode == KEY_D)
-        data->game.player.sidedirection = 1;
-    else if (keycode == KEY_ACTIVE_MOUSSE)
-		data->flage_mousse *= -1;
-    else if (keycode == KEY_ESC)
-        cleanup(data, 0);
-    return (0);
+void	init_movement(t_player *player, double *movestep, double *newplayerx,
+		double *newplayery)
+{
+	double	sideangle;
+
+	sideangle = player->rotationangle + M_PI / 2;
+	player->rotationangle = normalizeangle(player->rotationangle
+			+ player->turndirection * player->rotationspeed);
+	*(movestep) = player->walkdirection * player->movespeed;
+	*(newplayerx) = player->x + cos(player->rotationangle) * *(movestep);
+	*(newplayery) = player->y + sin(player->rotationangle) * *(movestep);
+	*newplayerx += cos(sideangle) * player->sidedirection * player->movespeed;
+	*newplayery += sin(sideangle) * player->sidedirection * player->movespeed;
 }
 
-int key_release(int keycode, t_data *data) {
-    if (!data) 
-        return (-1);
+int	key_press(int keycode, t_data *data)
+{
+	if (!data)
+		return (-1);
+	if (keycode == KEY_UP || keycode == KEY_W)
+		data->game.player.walkdirection = 1;
+	else if (keycode == KEY_DOWN || keycode == KEY_S)
+		data->game.player.walkdirection = -1;
+	else if (keycode == KEY_LEFT)
+		data->game.player.turndirection = -1;
+	else if (keycode == KEY_RIGHT)
+		data->game.player.turndirection = 1;
+	else if (keycode == KEY_A)
+		data->game.player.sidedirection = -1;
+	else if (keycode == KEY_D)
+		data->game.player.sidedirection = 1;
+	else if (keycode == KEY_ACTIVE_MOUSSE)
+		data->flage_mousse *= -1;
+	else if (keycode == KEY_ESC)
+		cleanup(data, 0);
+	return (0);
+}
 
-    if (keycode == KEY_UP || keycode == KEY_DOWN || keycode == KEY_W || keycode == KEY_S) {
-        data->game.player.walkdirection = 0;
-    }
-    else if (keycode == KEY_LEFT || keycode == KEY_RIGHT) {
-        data->game.player.turndirection = 0;
-    }
-    else if (keycode == KEY_A || keycode == KEY_D) {
-        data->game.player.sidedirection = 0;
-    }
-    return (0);
+int	key_release(int keycode, t_data *data)
+{
+	if (!data)
+		return (-1);
+	if (keycode == KEY_UP || keycode == KEY_DOWN || keycode == KEY_W
+		|| keycode == KEY_S)
+	{
+		data->game.player.walkdirection = 0;
+	}
+	else if (keycode == KEY_LEFT || keycode == KEY_RIGHT)
+	{
+		data->game.player.turndirection = 0;
+	}
+	else if (keycode == KEY_A || keycode == KEY_D)
+	{
+		data->game.player.sidedirection = 0;
+	}
+	return (0);
 }
 
 int	mouse_move(int x, int y, t_data *data)
